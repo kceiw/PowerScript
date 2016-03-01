c36450e312fca075955d035ddb63a865d92eb732
Maoliang <hellomao@outlook.com>
2016-02-29 22:46:15 -0800

    Add a grep-like PowerShell script.

---
+++ scripts/GrepShell.ps1
@@ -0,0 +1,21 @@
+﻿Param(
+    [Parameter(Mandatory=$True,Position=1)]
+    [String[]] $Pattern,
+    [String[]] $Path = ".",
+    [string[]] $Exclude,
+    [string[]] $Include,
+    [switch]$Recurse
+)
+
+$allChildItems = ""
+
+if ($Recurse)
+{
+    $allChildItems = Get-ChildItem -Recurse -Path $Path -Include $Include -Exclude $Exclude
+}
+else
+{
+    $allChildItems = Get-ChildItem -Path $Path -Include $Include -Exclude $Exclude
+}
+
+$allChildItems | Select-String -Pattern $Pattern
